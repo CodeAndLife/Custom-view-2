4098e0d2cf0dfe20d9ba7def6f89b7650ae0fdf5
Pavel Bobkov <microsmaster@rambler.ru>
2015-08-26 13:02:18 +0300

    first version of the project

--- app/src/main/java/com/example/android/customviewp2/MainActivity.java
+++ app/src/main/java/com/example/android/customviewp2/MainActivity.java
@@ -17,28 +17,6 @@ public class MainActivity extends ActionBarActivity {
         lineChart.setChartData(getRandomData());
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     private float[] getRandomData() {
         return new float[] { 10, 12, 7, 14, 15, 19, 13, 2, 10, 13, 13, 10, 15, 14 };
     }
